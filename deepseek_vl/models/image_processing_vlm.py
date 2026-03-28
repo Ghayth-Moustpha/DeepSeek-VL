6f8d885dde36e8956fdd279859e5105541dd1f39
ghayth.moustapha <ghayth.moustpha@gmail.com>
2026-03-28 20:28:05 +0300

    FIX VLMImageProcessorConfig

--- deepseek_vl/models/image_processing_vlm.py
+++ deepseek_vl/models/image_processing_vlm.py
@@ -54,16 +54,16 @@ def expand2square(pil_img, background_color):
 
 class VLMImageProcessorConfig(PretrainedConfig):
     model_type = "deepseek_vlm"
-    image_size: int
-    min_size: int
-    image_mean: Union[Tuple[float, float, float], List[float]]
-    image_std: Union[Tuple[float, float, float], List[float]]
-    rescale_factor: float
-    do_normalize: bool
+    image_size: int = 1024
+    min_size: int = 14
+    image_mean: Union[Tuple[float, float, float], List[float]] = IMAGENET_MEAN
+    image_std: Union[Tuple[float, float, float], List[float]] = IMAGENET_STD
+    rescale_factor: float = 1.0 / 255.0
+    do_normalize: bool = True
 
     def __init__(
         self,
-        image_size: int,
+        image_size: int = 1024,
         min_size: int = 14,
         image_mean: Union[Tuple[float, float, float], List[float]] = (
             0.48145466,
